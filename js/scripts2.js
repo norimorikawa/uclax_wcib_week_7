3ba4cb879e4818ce41eef406ee2c900a692a1b5f
norimorikawa <norikawa5385@gmail.com>
2021-06-02 20:53:45 -0700

    Week7 Rev1

--- js/scripts2.js
+++ js/scripts2.js
@@ -9,13 +9,13 @@ if ( my_switch == false ) {
 	my_switch = true;
 
 	$(this).html('It&apos;s a Girl').removeClass('btn-primary').addClass('btn-secondary');
-	$('h1').html('Congratulations!').removeClass('h1-pink').addClass('h1-blue');
+	$('h1').html('Congratulations on Your Baby Boy!');
 	$('#my_image').attr('src', 'images/boy3.png');
 } else {
 	my_switch = false;
 
 	$(this).html('It&apos;s a Boy').removeClass('btn-secondary').addClass('btn-primary');
-	$('h1').html('Congratulations!').removeClass('h1-blue').addClass('h1-pink');
+	$('h1').html('Congratulations on Your Baby Girl!');
 	$('#my_image').attr('src', 'images/girl3.png');
 }
 
@@ -32,10 +32,10 @@ if ( my_switch == false ) {
 
 //HIDE INSTRUCTIONS
 $(document).ready(function(){
-  $("#my_button").click(function(){
+  $("#hide").click(function(){
     $("p").hide();
   });
-  // $("#show").click(function(){
-  //   $("p").show();
-  // });
+  $("#show").click(function(){
+    $("p").show();
+  });
 });
